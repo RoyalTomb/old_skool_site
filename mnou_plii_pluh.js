67d149275f83b01dbca18b1eb1cd214414f7152b
RoyalTomb <68623530+RoyalTomb@users.noreply.github.com>
2024-08-29 03:47:58 -0400

    Add files via upload

---
+++ mnou_plii_pluh.js
@@ -0,0 +1,12 @@
+function content_toggle() {
+    var list = document.getElementById('content_list')
+    var button = document.getElementById('show_hide_button')
+
+    if (list.style.display == "none") {
+        list.style.display = "block";
+        button.innerHTML = '[Hide]'
+    } else {
+        list.style.display = "none";
+        button.innerHTML = '[Show]'
+    }
+}
